b29a33af00c1223da17d72438d4dfd665645724e
joel_patrick <jayjoelpn@gmail.com>
2024-09-09 09:16:59 +0300

    initial commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+ric = Dog('ric')
+print(ric.breed)
+
+snoopy = Dog('snoopy', "Mtina")
+print(snoopy.breed)
